673f859b4118071644a598f36a43f83f8b787841
lucasamui <lukeamui@hotmail.com>
2025-11-12 21:00:49 -0300

    final roteiro 3

--- modeloapp/settings.py
+++ modeloapp/settings.py
@@ -20,9 +20,11 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 APPS_DIR = os.path.join(BASE_DIR, 'apps') 
 sys.path.insert(0, APPS_DIR) 
+
+#APPS_DIR = os.path.join(BASE_DIR, 'apps') 
+#sys.path.insert(0, APPS_DIR) 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
